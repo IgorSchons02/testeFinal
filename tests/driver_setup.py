bba605a33a8b6b6660cf009f3f4b18ee40fb940f
IgorSchons02 <igorschons@ienh.com.br>
2024-11-12 19:10:26 -0300

    primeiro teste

---
+++ tests/driver_setup.py
@@ -0,0 +1,12 @@
+# driver_setup.py
+from selenium import webdriver
+import os
+
+def get_driver():
+    selenium_url = os.getenv('SELENIUM_REMOTE_URL', 'http://localhost:4444/wd/hub')
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=options
+    )
+    return driver
---
+++ tests/test_login.py
@@ -0,0 +1,15 @@
+# test_login.py
+from driver_setup import get_driver  # Importando a função get_driver
+from selenium.webdriver.common.by import By
+
+def test_login():
+    driver = get_driver()
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    # Simular ações no OrangeHRM
+    driver.find_element(By.ID, "txtUsername").send_keys("Admin")
+    driver.find_element(By.ID, "txtPassword").send_keys("admin123")
+    driver.find_element(By.ID, "btnLogin").click()
+
+    assert "dashboard" in driver.current_url
+    driver.quit()
