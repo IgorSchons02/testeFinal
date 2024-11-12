891f054653c23d14d613daa2c27374570386d6ee
IgorSchons02 <igorschons@ienh.com.br>
2024-11-12 20:53:36 -0300

    rub

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,27 @@
 # test_login.py
-from driver_setup import get_driver  # Importando a função get_driver
+from selenium import webdriver
+import time
+#from driver_setup import get_driver  # Importando a função get_driver
 from selenium.webdriver.common.by import By
 
-def test_login():
-    driver = get_driver()
-    driver.get("https://opensource-demo.orangehrmlive.com/")
+driver = webdriver.Chrome() 
+driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') 
+driver.maximize_window()
 
-    # Simular ações no OrangeHRM
-    driver.find_element(By.XPATH, "//a[text()='Username']").send_keys("Admin")
-    driver.find_element(By.XPATH, "//a[text()='passowrd']").send_keys("admin123")
-    driver.find_element(By.ID, "btnLogin").click()
-    
-    assert "dashboard" in driver.current_url
-    driver.quit()
+
+# Simular ações no OrangeHRM
+time.sleep(1)
+driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+time.sleep(1)
+driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+time.sleep(1)
+driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+
+time.sleep(1)
+
+assert "dashboard" in driver.current_url
+driver.quit()
+
+
+
 
