a80f9bab9341fdcb83db4598e5f541ff4252fbf3
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 12:57:20 -0300

    alteracao

--- tests/driver_setup.py
+++ tests/driver_setup.py
@@ -4,7 +4,7 @@ import os
 
 def get_driver():
     selenium_url = os.getenv('SELENIUM_REMOTE_URL', 'http://localhost:4444/wd/hub')
-    options = webdriver.ChromeOptions()
+    options = webdriver.FirefoxOptions()  # Alterado para FirefoxOptions
     driver = webdriver.Remote(
         command_executor=selenium_url,
         options=options
