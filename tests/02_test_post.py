7e40bba6b500c2ed93f4ef3ac7c770a3c32d8635
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 12:43:22 -0300

    test_post

---
+++ tests/02_test_post.py
@@ -0,0 +1,27 @@
+import time
+from driver_setup import get_driver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_user():
+    driver = get_driver()
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    #login
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/buzz/viewBuzz']").click()
+    time.sleep(10)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'What's on your mind?')]").send_keys("teste de post")
+    driver.find_element(By.XPATH, "//button[text()=' Post ']").click()
+
+    time.sleep(5)
+    assert "empNumber" in driver.current_url
+    driver.quit()
+
