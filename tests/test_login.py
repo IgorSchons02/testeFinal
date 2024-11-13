7164a69d258f4c94ed2cc66f3d02f0cf499149e7
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 08:15:29 -0300

    testerw

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,10 +5,17 @@ from selenium.webdriver.common.by import By
 def test_login():
     driver = get_driver()
     driver.get("https://opensource-demo.orangehrmlive.com/")
-    
-    #login 
+           
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
     driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
 
+try:
+    driver = get_driver()
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    assert "dashboard" in driver.current_url
+    print("TESTE APROVADO")  # Se o assert for true
+except AssertionError:
+    print("TESTE REPROVADO")  
 
+    driver.quit()
