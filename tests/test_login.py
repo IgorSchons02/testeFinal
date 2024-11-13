59e26d6fb887f538d12bffb8eab2005db45c8caf
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 08:30:56 -0300

    run

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,12 +5,18 @@ from selenium.webdriver.common.by import By
 def test_login():
     driver = get_driver()
     driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    #login
     time.sleep(2)
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
     time.sleep(1)
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
     time.sleep(1)
     driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "a.hrefch[href='/web/index.php/pim/viewPimModule']").click()
 
-    assert "dashboard" in driver.current_url
+    driver.find_element(By.XPATH, "//input[@type='button']")
+
+    assert "addEmployee" in driver.current_url
     driver.quit()
