9a0f01d82fccc1f905a3f7ab53026e570399dd23
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 08:22:45 -0300

    run

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,9 +5,11 @@ from selenium.webdriver.common.by import By
 def test_login():
     driver = get_driver()
     driver.get("https://opensource-demo.orangehrmlive.com/")
-           
+    time.sleep(2)
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+    time.sleep(1)
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    time.sleep(1)
     driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
 
     assert "dashboard" in driver.current_url
