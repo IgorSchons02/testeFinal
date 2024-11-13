3e36fabdbe2c5ea4a43d9454d7bed09df044fe7d
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 13:03:00 -0300

    teste

--- tests/test_user.py
+++ tests/test_user.py
@@ -35,3 +35,24 @@ def test_user():
     assert "empNumber" in driver.current_url
     driver.quit()
 
+def test_post():
+    driver = get_driver()
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    #login
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/buzz/viewBuzz']").click()
+    time.sleep(10)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'What's on your mind?')]").send_keys("teste de post")
+    driver.find_element(By.XPATH, "//button[text()=' Post ']").click()
+
+    time.sleep(5)
+    assert "viewBuzz" in driver.current_url
+    driver.quit()
