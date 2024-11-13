273c235de594fc3d107852918e676f1cc1d6e9a2
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 17:08:32 -0300

    teste_new_02

--- tests/test_user.py
+++ tests/test_user.py
@@ -240,5 +240,28 @@ def test_postarVideo():
     driver.find_element(By.ID, 'oxd-toaster_1') #id da confirmação
     driver.quit()
 
+def test_deletarPost():
+    driver = get_driver()
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    #login
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/buzz/viewBuzz']").click()
+    time.sleep(2)
+    driver.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[2]/li/button/i").click()
+    time.sleep(2)
+    driver.find_element(By.XPATH, "/html/body/div/div[1]/div[2]/div[2]/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[2]/li/ul/li[1]/p").click()
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//button[text()=' Yes, Delete ']").click()
+
+    driver.find_element(By.ID, 'oxd-toaster_1') #id da confirmação
+    driver.quit()
 
 
