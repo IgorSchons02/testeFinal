7232266cd80d8c33c86e8efb94fa5b0a8b614144
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 09:16:36 -0300

    run

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,29 +1,24 @@
+import time
+from driver_setup import get_driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from driver_setup import get_driver
 
 def test_login():
     driver = get_driver()
     driver.get("https://opensource-demo.orangehrmlive.com/")
 
-    # login
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@placeholder,'Username')]"))).send_keys("Admin")
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@placeholder,'Password')]"))).send_keys("admin123")
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Login ']"))).click()
+    #login
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/pim/viewPimModule']").click()
+    time.sleep(200)
+    driver.find_element(By.CSS_SELECTOR, "button.oxd-button.oxd-button--medium.oxd-button--secondary").click()
 
-    # Clica no menu "PIM"
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/pim/viewPimModule']"))).click()
-
-    assert "viewEmployeeList" in driver.current_url
+    assert "addEmployee" in driver.current_url
     driver.quit()
-
-    # # Espera pelo botão "Add" e clica nele
-    # button_add = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.oxd-button.oxd-button--medium.oxd-button--secondary")))
-    # driver.execute_script("arguments[0].click();", button_add)
-
-    # WebDriverWait(driver, 30).until(EC.url_contains("addEmployee"))
-
-    # # Verificação de URL
-    # assert "addEmployee" in driver.current_url
-    # driver.quit()
