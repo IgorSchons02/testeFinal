b6e3f75171d0e1430a8ce99685655224b5033e2f
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 08:06:20 -0300

    tes

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,7 +10,7 @@ def test_login():
     time.sleep(1)
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
     time.sleep(1)
-    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("23")
     time.sleep(1)
     driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
 
