6d2063fc070ddc7b3e6df662b734ea7620610f95
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 08:57:35 -0300

    run

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,26 +1,23 @@
-import time
-from driver_setup import get_driver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from driver_setup import get_driver
 
 def test_login():
     driver = get_driver()
     driver.get("https://opensource-demo.orangehrmlive.com/")
 
-    #login
-    time.sleep(2)
-    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
-    time.sleep(1)
-    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
-    time.sleep(1)
-    driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
-    time.sleep(2)
+    # login
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@placeholder,'Username')]"))).send_keys("Admin")
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[contains(@placeholder,'Password')]"))).send_keys("admin123")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[text()=' Login ']"))).click()
+
+    # Clica no menu "PIM"
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/pim/viewPimModule']"))).click()
-    
+
+    # Espera pelo botão "Add" e clica nele
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.oxd-button.oxd-button--medium.oxd-button--secondary"))).click()
 
+    # Verificação de URL
     assert "addEmployee" in driver.current_url
     driver.quit()
-
-
