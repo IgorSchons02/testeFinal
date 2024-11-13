122abb2865f783fe8583291900f80870e07c79f3
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 08:13:37 -0300

    run

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,20 +6,9 @@ def test_login():
     driver = get_driver()
     driver.get("https://opensource-demo.orangehrmlive.com/")
     
-    # Simular ações no OrangeHRM
-    time.sleep(1)
+    #login 
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
-    time.sleep(1)
     driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
-    time.sleep(1)
     driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
 
-try:
-    driver = get_driver()
-    driver.get("https://opensource-demo.orangehrmlive.com/")
-    assert "dashboard" in driver.current_url
-    print("TESTE APROVADO")  # Se o assert for true
-except AssertionError:
-    print("TESTE REPROVADO")  
 
-    driver.quit()
