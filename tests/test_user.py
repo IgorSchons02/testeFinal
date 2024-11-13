c2640080876295e87bd7bc08e1a93654ba63e18a
IgorSchons02 <igorschons@ienh.com.br>
2024-11-13 14:07:51 -0300

    teste_curtida

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,6 @@
 import time
 from driver_setup import get_driver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 def test_user():
     driver = get_driver()
@@ -35,10 +34,6 @@ def test_user():
     assert "empNumber" in driver.current_url
     driver.quit()
 
-import time
-from driver_setup import get_driver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 def test_post():
     driver = get_driver()
@@ -63,3 +58,23 @@ def test_post():
     driver.quit()
 
 
+def test_curtida():
+    driver = get_driver()
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    #login
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Username')]").send_keys("Admin")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//input[contains(@placeholder,'Password')]").send_keys("admin123")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "//button[text()=' Login ']").click()
+
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, "a.oxd-main-menu-item[href='/web/index.php/buzz/viewBuzz']").click()
+    time.sleep(10)
+    driver.find_element(By.ID, "heart").click()
+    
+    time.sleep(5)
+    assert "viewBuzz" in driver.current_url
+    driver.quit()
